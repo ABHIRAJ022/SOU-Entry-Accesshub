56af06015786f44cefe786e3f4bf1f535087926a
GOHIL DIPESH <2301031000023@silveroakuni.ac.in>
2026-08-29 10:22:19 +0000

    Include Codespaces forwarded origin in CSRF_TRUSTED_ORIGINS for development to avoid CSRF failures when using app.github.dev forwarding

--- config/settings_local.py
+++ config/settings_local.py
@@ -1,4 +1,26 @@
 from .settings import *
+import os
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEBUG = True
 SECURE_SSL_REDIRECT = False
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = [
+	'http://localhost:8000', 'https://localhost:8000',
+	'http://127.0.0.1:8000', 'https://127.0.0.1:8000',
+	'http://localhost:8001', 'https://localhost:8001',
+	'http://127.0.0.1:8001', 'https://127.0.0.1:8001',
+]
+# If running inside GitHub Codespaces / forwarded app domain, add the forwarding origin so CSRF checks pass
+codespace_name = os.getenv('CODESPACE_NAME')
+codespace_domain = os.getenv('GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN', 'app.github.dev')
+if codespace_name:
+    codespace_host = f'{codespace_name}-8000.{codespace_domain}'
+    codespace_origin = f'https://{codespace_host}'
+    if codespace_origin not in CSRF_TRUSTED_ORIGINS:
+        CSRF_TRUSTED_ORIGINS.append(codespace_origin)
+
+DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3'}}
+STORAGES = {
+	**STORAGES,
+	'staticfiles': {'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage'},
+}
