d10caa47fb21d1cdd78112475a3d9d88827b412c
ABHIRAJ022 <rajput12557q@gmail.com>
2026-09-01 16:17:44 +0000

    p

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,4 +1,23 @@
+import logging
 import os
-from django.core.wsgi import get_wsgi_application
+
+logger = logging.getLogger(__name__)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+
+def ensure_database_migrations():
+    try:
+        import django
+        from django.core.management import call_command
+        django.setup()
+        call_command('migrate', interactive=False, verbosity=0, run_syncdb=True)
+    except Exception:
+        logger.exception('Automatic database migration failed during startup.')
+        raise
+
+
+ensure_database_migrations()
+
+from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
